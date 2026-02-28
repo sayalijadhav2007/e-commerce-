ad4a6ec43880defedf66980e30ed3f44afaf96d4
Sayali Jadhav <sayalijadhav2007@gmail.com>
2026-02-28 17:50:10 +0530

    fix base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1066566752" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3737857366" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
